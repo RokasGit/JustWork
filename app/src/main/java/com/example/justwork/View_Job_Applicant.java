19a396f1c7fae05caec48524a51d94b1798e3185
Nichita Railean <nichita.railean@aol.com>
2022-04-21 16:02:42 +0300

    Job Applicant Window XML done

--- app/src/main/java/com/example/justwork/View_Job_Applicant.java
+++ app/src/main/java/com/example/justwork/View_Job_Applicant.java
@@ -11,4 +11,9 @@ import android.view.ViewGroup;
 
 public class View_Job_Applicant extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_view__job__applicant, container, false);
+    }
+
 }
