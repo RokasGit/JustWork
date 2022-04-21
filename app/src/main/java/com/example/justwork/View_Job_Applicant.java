2508c3bb8303d0a71d5f0142b7a4d04eb5e84b8f
Nichita Railean <nichita.railean@aol.com>
2022-04-21 15:40:39 +0300

    Job Applicant half Window XML done

---
+++ app/src/main/java/com/example/justwork/View_Job_Applicant.java
@@ -0,0 +1,14 @@
+package com.example.justwork;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class View_Job_Applicant extends Fragment {
+
+}
