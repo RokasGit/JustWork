66f9358f578605d449ff079a159ac084a76cf536
Nichita Railean <nichita.railean@aol.com>
2022-04-28 15:12:15 +0300

    Company Home almost Done

--- app/src/main/java/com/example/justwork/view/company_home.java
+++ app/src/main/java/com/example/justwork/view/company_home.java
@@ -2,65 +2,74 @@ package com.example.justwork.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.justwork.Adapters.JobAdapter;
 import com.example.justwork.R;
+import com.example.justwork.viewmodel.CompanyViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link company_home#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class company_home extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private CompanyViewModel viewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public company_home() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment company_home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static company_home newInstance(String param1, String param2) {
-        company_home fragment = new company_home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText searchBar;
+    ImageView menuButton;
+    ImageView filterOptions;
+    ImageView companyLogo;
+    Button postAJob;
+    RecyclerView JobRecyclerView;
+    JobAdapter jobAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_company_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = new ViewModelProvider(this).get(CompanyViewModel.class);
+        searchBar = (EditText) getView().findViewById(R.id.company_home_search);
+        menuButton = (ImageView) getView().findViewById(R.id.Company_home_imageView);
+        companyLogo = (ImageView) getView().findViewById(R.id.company_home_company_logo_img);
+        filterOptions = (ImageView) getView().findViewById(R.id.company_home_imageView2);
+        postAJob = (Button) getView().findViewById(R.id.company_home_button_postJob);
+        JobRecyclerView = (RecyclerView) getView().findViewById(R.id.company_home_Recycler);
+
+        jobAdapter = new JobAdapter(viewModel.getJobPostings());
+
+        jobAdapter.setOnClickListener(job -> {
+            //something
+        });
+
+        JobRecyclerView.setAdapter(jobAdapter);
+    }
+
+    public void filterOptions(View view){
+
+    }
+
+    public void PostAJob(View view){
+
+    }
+
+    public void MenuButton(View view){
+
+    }
+
+
 }
--- app/src/main/java/com/example/justwork/viewmodel/CompanyViewModel.java
+++ app/src/main/java/com/example/justwork/viewmodel/CompanyViewModel.java
@@ -1,6 +1,28 @@
 package com.example.justwork.viewmodel;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
-public class CompanyViewModel extends ViewModel {
+import com.example.justwork.model.Job;
+
+import java.util.ArrayList;
+
+public class CompanyViewModel extends AndroidViewModel {
+
+    private ArrayList<Job> jobPostings;
+
+    public CompanyViewModel(Application application){
+        super(application);
+        jobPostings = new ArrayList<>();
+    }
+
+    public ArrayList<Job> getJobPostings() {
+        return jobPostings;
+    }
+
+    public void addJob(Job job) {
+        jobPostings.add(job);
+    }
 }
