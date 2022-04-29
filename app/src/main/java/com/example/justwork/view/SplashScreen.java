0eed0c54cbdf7223f27316236f6555d7992e0ad6
Rokas <43143523+RokasGit@users.noreply.github.com>
2022-04-29 16:07:55 +0200

    Navigation for employee(user) done.

--- app/src/main/java/com/example/justwork/view/SplashScreen.java
+++ app/src/main/java/com/example/justwork/view/SplashScreen.java
@@ -3,19 +3,34 @@ package com.example.justwork.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.justwork.R;
 
 
 public class SplashScreen extends Fragment {
-
+    private NavController navController;
+    private Button splashButton;
+    private View splashView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+        splashView = inflater.inflate(R.layout.fragment_splash_screen, container, false);
+        setupNavigation();
+        initViews();
+        return splashView;
     }
-
+    private void setupNavigation(){
+        navController = NavHostFragment.findNavController(this);
+    }
+    private void initViews(){
+        splashButton = splashView.findViewById(R.id.splash_button);
+        splashButton.setOnClickListener(view -> navController.navigate(R.id.nav_logout));
+    }
 }
