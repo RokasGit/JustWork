6bc1b5f956b6db7a80ff73b48fd3a4647abaf76c
rokasgit <ifamous.rp@gmail.com>
2022-05-09 16:35:21 +0200

    set company home as top level destination to get access to menu.

--- app/src/main/java/com/example/justwork/view/MainActivity.java
+++ app/src/main/java/com/example/justwork/view/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_edit_profile, R.id.nav_applications, R.id.nav_notification_settings, R.id.employeeHomeFragment)
+                R.id.nav_edit_profile, R.id.nav_applications, R.id.nav_notification_settings,R.id.employeeHomeFragment, R.id.company_home)
                 .setOpenableLayout(drawerLayout)
                 .build();
        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
